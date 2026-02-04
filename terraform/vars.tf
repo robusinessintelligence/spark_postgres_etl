27268d66d921c047c9e3bd6cec84c3b6413dddd5
Raphael Oliveira <raphaelbrf@gmail.com>
2026-02-03 22:13:48 -0300

    pipeline ci_cd

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,8 +2,6 @@
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
 # Any changes made to this file may cause irreparable errors to the infrastructure created
-# by Terraform. Only make changes if you are CERTAIN of what you are doing. Test changes
-# in the dev environment whenever you HAVE to make them.
 ###################################################################################################
 
 locals {
@@ -21,16 +19,6 @@ variable "env" {
   type        = string
   description = "Environment of the code"
 }
-
-# variable "project_hash" {
-#   type        = string
-#   description = "Hash of the project name"
-# }
-
-# variable "pipeline_project" {
-#   type        = string
-#   description = "Google Cloud project identifier for the pipeline"
-# }
 
 variable "data_project" {
   type        = string
@@ -56,39 +44,3 @@ variable "solution" {
   type        = string
   description = "Project solution name"
 }
-
-# variable "subnet" {
-#   type        = string
-#   description = "Project subnetwork"
-# }
-
-# variable "service_account" {
-#   type        = string
-#   description = "Service account being used"
-# }
-
-# variable "testing" {
-#   type        = bool
-#   description = "Whether this execution is for a test"
-#   default     = false
-# }
-
-# variable "vpc" {
-#   type        = string
-#   description = "VPC used by the project's Vertex AI"
-# }
-
-# variable "git_repository" {
-#   type        = string
-#   description = "GitLab repository for the solution"
-# }
-
-# variable "composer_dags_team" {
-#   type        = string
-#   description = "Team responsible for Composer DAGs"
-# }
-
-# variable "composer_bucket" {
-#   type        = string
-#   description = "GCS Bucket for Composer DAGs"
-# }
